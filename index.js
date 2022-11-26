df75ea5f7dd43d64fae30a9b94e39683b736083a
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-11-26 14:10:30 +0600

    Categories get and create completed

--- index.js
+++ index.js
@@ -13,16 +13,37 @@ app.use(express.json());
 
 // Database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f3zh5ao.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("carhub").collection("categories");
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
 });
+
+async function run() {
+  try {
+    const categoriesCollection = client.db('carhub').collection('categories');
+    // Get all categories
+    app.get('/categories', async (res, req) => {
+      const categories = await categoriesCollection.find({}).toArray();
+      res.send(categories);
+    });
+
+    // Create category
+    app.post('/category', async (res, req) => {
+      const category = req.body;
+      const result = await categoriesCollection.insertOne(data);
+      res.send(result);
+    });
+  } finally {
+
+  }
+}
+
+run().catch(e => console.error(e));
 
 
 app.get('/', (req, res) => {
-    res.send("Carhub server is running");
+  res.send("Carhub server is running");
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
